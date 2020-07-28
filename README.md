58b0ba70aaeb53c5e56d1b48e655b37175796084
Diego Sesmas <diegos7167@gmail.com>
2020-07-27 19:57:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Oceanic
+> Wield the wonders of the sea
+
+### About
+My friend Caleb and I are working on this for development experience and for fun.
+
+## Plans
+We plan to start with a few decorative blocks and soon add more functional ones.
---
+++ src/main/kotlin/com/github/diego7167/Oceanic.kt
@@ -0,0 +1,12 @@
+package com.github.diego7167
+
+import net.fabricmc.api.ModInitializer
+
+// For support join https://discord.gg/v6v4pMv
+
+@Suppress("unused")
+class Oceanic: ModInitializer {
+    override fun onInitialize() {
+        println("Wield the wonders of the sea")
+    }
+}
