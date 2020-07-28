ccad27c4dda7041042260a0a79ce77226d15d85e
Diego Sesmas <diegos7167@gmail.com>
2020-07-27 20:25:47 -0700

    Added base item and itemgroup

--- src/main/kotlin/com/github/diego7167/Oceanic.kt
+++ src/main/kotlin/com/github/diego7167/Oceanic.kt
@@ -1,12 +1,25 @@
 package com.github.diego7167
 
 import net.fabricmc.api.ModInitializer
-
-// For support join https://discord.gg/v6v4pMv
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
 
 @Suppress("unused")
 class Oceanic: ModInitializer {
+    private val oceanicItemGroup: ItemGroup = FabricItemGroupBuilder.build(
+            Identifier("oceanic", "general"),
+            fun(): ItemStack = ItemStack(purePrismarine)
+    )
+
+    private val purePrismarine = Item(Item.Settings().group(oceanicItemGroup))
+
     override fun onInitialize() {
         println("Wield the wonders of the sea")
+
+        Registry.register(Registry.ITEM, Identifier("oceanic", "pure_prismarine"), purePrismarine)
     }
 }
