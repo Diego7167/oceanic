a60027fb16a4c77b9709ff9edde9fef3fe304225
Diego Sesmas <diegos7167@gmail.com>
2020-07-29 14:39:44 -0700

    Made pure crystal block rotatable because the top part looks nice

---
+++ src/main/kotlin/com/github/diego7167/blocks/PureCrystalBlock.kt
@@ -0,0 +1,23 @@
+package com.github.diego7167.blocks
+
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.block.HorizontalFacingBlock
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.Properties
+import net.minecraft.util.math.Direction
+
+class PureCrystalBlock(settings: Settings): HorizontalFacingBlock(settings) {
+    init {
+        super.setDefaultState(this.stateManager.defaultState.with(Properties.HORIZONTAL_FACING, Direction.NORTH))
+    }
+
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>?) {
+        builder?.add(Properties.HORIZONTAL_FACING)
+    }
+
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        return this.defaultState.with(FACING, ctx.playerFacing)
+    }
+}
