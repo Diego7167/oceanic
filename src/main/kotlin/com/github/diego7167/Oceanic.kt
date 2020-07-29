4467bce53d5c4b7b60865a12bc79f7abd90196b0
Diego Sesmas <diegos7167@gmail.com>
2020-07-29 15:12:45 -0700

    Added ore but it doesn't generate

--- src/main/kotlin/com/github/diego7167/Oceanic.kt
+++ src/main/kotlin/com/github/diego7167/Oceanic.kt
@@ -2,6 +2,7 @@ package com.github.diego7167
 
 import com.github.diego7167.blocks.PureCrystalBlock
 import com.github.diego7167.blocks.PurePrismarineLantern
+import com.github.diego7167.blocks.ShinyGravel
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
@@ -20,7 +21,7 @@ class Oceanic: ModInitializer {
 	// Item group
 	private val oceanicItemGroup: ItemGroup = FabricItemGroupBuilder.build(
 		Identifier("oceanic", "general"),
-		fun(): ItemStack = ItemStack(purePrismarine)
+		fun(): ItemStack = ItemStack(pureCrystal)
 	)
 
 	// Items
@@ -32,6 +33,10 @@ class Oceanic: ModInitializer {
 	private val purePrismarineBlock = Block(FabricBlockSettings.of(Material.STONE).hardness(5.0f))
 	private val pureCrystalBlock = PureCrystalBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f))
 	private val purePrismarineLantern = PurePrismarineLantern(FabricBlockSettings.of(Material.GLASS).hardness(0.5f).lightLevel(15).sounds(BlockSoundGroup.GLASS))
+	// Ores should be public and companions
+	companion object Ores {
+		val shinyGravel = ShinyGravel(FabricBlockSettings.of(Material.SOIL).hardness(0.6f))
+	}
 
 	// Init
 	override fun onInitialize() {
@@ -46,9 +51,11 @@ class Oceanic: ModInitializer {
 		Registry.register(Registry.BLOCK, Identifier("oceanic", "pure_prismarine_block"), purePrismarineBlock)
 		Registry.register(Registry.BLOCK, Identifier("oceanic", "pure_crystal_block"), pureCrystalBlock)
 		Registry.register(Registry.BLOCK, Identifier("oceanic", "pure_prismarine_lantern"), purePrismarineLantern)
+		Registry.register(Registry.BLOCK, Identifier("oceanic", "shiny_gravel"), shinyGravel)
 		// And ItemBlocks
 		Registry.register(Registry.ITEM, Identifier("oceanic", "pure_prismarine_block"), BlockItem(purePrismarineBlock, Item.Settings().group(oceanicItemGroup)))
 		Registry.register(Registry.ITEM, Identifier("oceanic", "pure_crystal_block"), BlockItem(pureCrystalBlock, Item.Settings().group(oceanicItemGroup)))
 		Registry.register(Registry.ITEM, Identifier("oceanic", "pure_prismarine_lantern"), BlockItem(purePrismarineLantern, Item.Settings().group(oceanicItemGroup)))
+		Registry.register(Registry.ITEM, Identifier("oceanic", "shiny_gravel"), BlockItem(shinyGravel, Item.Settings().group(oceanicItemGroup)))
 	}
 }
---
+++ src/main/kotlin/com/github/diego7167/blocks/ShinyGravel.kt
@@ -0,0 +1,5 @@
+package com.github.diego7167.blocks
+
+import net.minecraft.block.FallingBlock
+
+class ShinyGravel(settings: Settings): FallingBlock(settings) {}
